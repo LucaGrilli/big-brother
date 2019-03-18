d52332170da6b4200ba821cbee23fa590583426d
davidemonnati <davide.monnati@gmail.com>
2019-03-18 18:20:00 +0100

    add create user dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsInt, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  readonly email: string;
+
+  @IsInt()
+  readonly phone: number;
+}
