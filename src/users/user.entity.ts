06dfce2a4bc30a70c30d7d83cfdadbbd02969582
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-03-19 15:21:33 +0100

    change decorators on user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,32 +1,28 @@
-import { Entity, Column, ObjectIdColumn, ObjectID } from 'typeorm';
+import { Entity, Column, ObjectIdColumn, ObjectID, Index } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
 
 @Entity()
 export class User {
     @ObjectIdColumn()
     id: ObjectID;
 
-    @Column({
-        length:30,
-        unique: true,
-    })
+    @Column({ length: 50 })
+    @IsNotEmpty()
+    @Index({ unique: true })
     name: string;
 
-    @Column({
-        length: 30,
-        unique: true,
-    })
+    @Column({ length: 100 })
+    @IsNotEmpty()
+    @Index({unique: true})
     email: string;
 
-    @Column({ length: 50 })
+    @Column({ length: 30 })
     firstname: string;
-
-    @Column({ length: 50 })
+    
+    @Column({ length: 30 })
     lastname: string;
 
-    @Column({
-        unique: true,
-        nullable: true,
-    })
+    @Column()
     phone: number;
 
     constructor(name: string, email: string, firstname: string, lastname: string){
