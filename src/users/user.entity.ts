2d4db1780b9df611ce1a1b3b85f250f787f6005a
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-03-21 12:51:49 +0100

    remove properties (not work properly with typeorm)

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, Column, ObjectIdColumn, ObjectID, Index } from 'typeorm';
-import { IsNotEmpty, IsOptional, IsString, IsPhoneNumber, IsEmail, IsAlpha, MaxLength, IsMongoId } from 'class-validator';
+import { IsOptional, IsString, IsPhoneNumber, IsEmail, IsAlpha, MaxLength, IsMongoId } from 'class-validator';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Entity('users')
@@ -7,59 +7,27 @@ export class User {
     @ObjectIdColumn()
     @IsMongoId()
     public _id: ObjectID;
-    
-    private _username: string;
-    private _email: string;
-    private _fullname: string;
-    private _phone: number;
 
-    public get username(): string {
-        return this._username;
-    }
+    @Column()
+    @Index({ unique: true})
+    @MaxLength(30)
+    @IsString()
+    public username: string;
 
-    public set username(username: string){
-        if(username == null){
-            throw new HttpException(
-                'Username cannot be null',
-                HttpStatus.BAD_REQUEST,
-            );
-        }
-        this._username = username;
-    }
+    @Column()
+    @Index({ unique: true})
+    @MaxLength(70)
+    @IsEmail()
+    public email: string;
 
-    public get email(): string {
-        return this._email;
-    }
+    @Column()
+    @MaxLength(100)
+    @IsString()
+    @IsAlpha()
+    public fullname: string;
 
-    public set email(email: string){
-        if(email == null){
-            throw new HttpException(
-                'Email cannot be null',
-                HttpStatus.BAD_REQUEST,
-            );
-        }
-        this._email = email.toLowerCase();
-    }
-
-    public get fullname(): string {
-        return this._fullname;
-    }
-
-    public set fullname(fullname: string){
-        if(fullname == null){
-            throw new HttpException(
-                'Fullname cannot be null',
-                HttpStatus.BAD_REQUEST,
-            );
-        }
-        this._fullname = fullname;
-    }
-
-    public get phone(): number {
-        return this._phone;
-    }
-
-    public set phone(phone: number){
-        this._phone = phone;
-    }
+    @Column()
+    @IsOptional()
+    @IsPhoneNumber("IT")
+    public phone: number;
 }
