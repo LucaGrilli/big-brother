647bbb16c81f596278199d72f2cad0f45da7a65f
LucaGrilli <luca.grilli@studenti.unicam.it>
2019-03-25 16:15:16 +0100

    creazione devices e relativicampi

---
+++ src/devices/device.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, ObjectIdColumn, ObjectID, Column, Index } from "typeorm";
+import { Role } from "src/users/enums/role.enum";
+import { IPv4 } from "ip-num/IPv4";
+@Entity('devices')
+export class devices {
+    @ObjectIdColumn()
+    public _id: ObjectID;
+
+    @Column()
+    @Index({ unique: true})
+    public ip:IPv4;
+
+    @Column()
+    @Index({ unique: true})
+    public macaddress: string;
+
+    @Column()
+    public name: string;
+
+    
+    constructor(ip: IPv4, macaddress:string, name: string){
+        this.ip = ip;
+        this.macaddress = macaddress;
+        this.name = name;
+        
+    }
+}
