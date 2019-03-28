d05cfb71fb7b94f74874951a3c6dc13aa0e3ee0d
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-03-28 16:23:36 +0100

    add user service

---
+++ src/modules/users/services/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "../entities/user.entity";
+import { UserDto } from "../dto/user.dto";
+
+@Injectable()
+export class UsersService{
+    constructor(
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>,
+    ) {}
+
+    async create(userDto: UserDto){
+        await this.usersRepository.save(new User(
+            userDto.username,
+            userDto.email,
+            userDto.fullname,
+            userDto.phone,
+        ))
+    }
+
+    async delete(username: string){
+        await this.usersRepository.delete({
+            username: username,
+        })
+    }
+}
